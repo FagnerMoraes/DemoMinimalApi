6d69d4286497f733c6cd62f0eea9e7ba26263335
Fagner Moraes <fagm.carvalho@gmail.com>
2022-10-04 22:19:06 -0300

    Adicionar arquivos de projeto.

---
+++ DemoMinimalApi/Data/MinimalContextDb.cs
@@ -0,0 +1,35 @@
+﻿using DemoMinimalApi.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMinimalApi.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Fornecedor>()
+                .ToTable("TB_Fornecedor");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
