9e58447510afdfbabce5291ceed22d7997754de2
Fagner Moraes <fagm.carvalho@gmail.com>
2022-10-04 22:49:01 -0300

    improve api

--- DemoMinimalApi/Program.cs
+++ DemoMinimalApi/Program.cs
@@ -1,4 +1,5 @@
 using DemoMinimalApi.Data;
+using DemoMinimalApi.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,5 +19,22 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/fornecedores", async (
+    MinimalContextDb context) => 
+    await context.Fornecedores.ToListAsync())
+    .WithName("GetFornecedores")
+    .WithTags("Fornecedor");
+
+app.MapGet("/fornecedores/{id}", async (
+    Guid id, 
+    MinimalContextDb context) =>
+
+    await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor
+        ? Results.Ok(fornecedor)
+        : Results.NotFound())
+    .WithName("GetFornecedoresPorId")
+    .WithTags("Fornecedor");
 
 app.Run();
